af9913d604c0a73e1e4d1d9980290b422a48368b
alicepisani <alice.pisani@gmail.com>
2023-07-19 08:48:52 -0400

    update

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,10 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
+    "sphinx_tabs.tabs",
+    "nbsphinx",
+    "sphinx_gallery.load_style",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,6 +45,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
